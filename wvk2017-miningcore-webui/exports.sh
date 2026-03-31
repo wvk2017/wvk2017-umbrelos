6ba19e007ece43d2dc628814c379bde0805ac5dd
wvk2017 <58895020+wvk2017@users.noreply.github.com>
2026-03-31 15:02:54 +0200

    Add exports.sh to create directories and set permissions

---
+++ wvk2017-miningcore-webui/exports.sh
@@ -0,0 +1,4 @@
+mkdir -p /home/umbrel/.miningcore
+mkdir -p /home/umbrel/umbrel/app-data/wvk2017-vtc-node/.vertcoin
+chown -R 1000:1000 /home/umbrel/.miningcore
+chown -R 1000:1000 /home/umbrel/umbrel/app-data/wvk2017-vtc-node/.vertcoin
